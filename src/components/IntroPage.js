1bf83930ff2cae09b6e7228b81381a0e3715c2e3
Antonio Sala <59183374+Antho137@users.noreply.github.com>
2024-09-09 01:00:35 +0100

    Update IntroPage.js

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -9,7 +9,7 @@ const IntroPage = () => {
             </header>
             <div className='intro'>
                 <h2>About the Program</h2>
-                <p>The professor enter the record for each student in a class which allow him to evaluate the results by displaying some statistics.</p>
+                <p>The professor enters the record for each student in a class which allows him to perform an evaluation.</p>
             </div>
         </div>
     );
